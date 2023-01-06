90e06eb7cca0aff7d10e920f1f17a5cbf23737c1
Fabian Blasch <blasch57@gmail.com>
2023-01-06 19:58:01 +0100

    removed incomplete cols and managed to run spatial tests

--- Code/02_models.R
+++ Code/02_models.R
@@ -2,7 +2,7 @@
 source("./Code/Auxilliary.R")
 
 # packages
-get.package(c("splm", "spatialreg", "Ecdat"))
+get.package(c("splm", "spatialreg", "Ecdat", "spdep"))
 
 # data
 df <- readRDS("./Data/China_Sourced/rds/df_cut_years.rds")
@@ -17,24 +17,26 @@ invd1 <- lapply(distances, \(x) (1 / x))
 Wlist <- nb2listw(nb.dist.band, glist = invd1, style = "B")
 
 # W matrix KNN
-coords <- st_coordinates(st_centroid(df[df$year == 2011, ]$Geom))
-k.near <- spdep::knearneigh(coords, k = 3)
-k3 <- spdep::knn2nb(k.near)
-Wlist <- spdep::nb2listw(k3, style = "W")
+# coords <- st_coordinates(st_centroid(df[df$year == 2011, ]$Geom))
+# k.near <- spdep::knearneigh(coords, k = 3)
+# k3 <- spdep::knn2nb(k.near)
+# Wlist <- spdep::nb2listw(k3, style = "W")
 
-# build formula
-!colnames(df) %in% c("Region", "Geom", "year", "Number_of_Beds_in_Health_Care_Institutions",
+# model df without geom
+df_model <- df[, -2]
+
+# build indices
+!colnames(df_model) %in% c("Region", "Geom", "year", "Number_of_Beds_in_Health_Care_Institutions",
                      "Number_of_Beds_in_Hospitals", "Number_of_Health_Care_Institutions", "Number_of_Medical_Personell",
                      "Sample_population_of_age_15_65", "Waste_Gas_Emissions_Smoke_and_Dust", "CPI_Health_Care",
-                     "Health_Care_Expenditures") -> excl_ind
+                     "Health_Care_Expenditures") -> incl_ind
+# NAs
+sapply(df_model, \(cols) complete.cases(cols) |> all()) -> miss_ind
 
-fml <- paste("Health_Care_Expenditures ~", paste(colnames(df)[excl_ind], collapse = " + ")) |> as.formula()
-
-# Is there a spatial Lag structure?
-df2 <- df[, -2] # tried remopving geom as error was received
-df2[, "Region"] <- as.factor(df2[, "Region"]) # tried classes for region
+# build formula
+fml <- paste("Health_Care_Expenditures ~", paste(colnames(df_model)[incl_ind & miss_ind], collapse = " + ")) |> as.formula()
 
 # first test
-slmlag <- slmtest(fml, data = df2, listw = Wlist, test = "lml", model = "within")
+slmlag <- splm::slmtest(fml, data = df_model, listw = Wlist, test = "lml")
 
 

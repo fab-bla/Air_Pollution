8d2eb1e9a562570f093379180afef891a0c1227c
Fabian Blasch <blasch57@gmail.com>
2023-02-02 18:33:08 +0100

    final model update

--- Code/02_models.R
+++ Code/02_models.R
@@ -47,7 +47,32 @@ df_panel <- cbind(df_panel, lagged_vars) |> plm::pdata.frame(index = c("Region",
                      "Health_Care_Expenditures", "Consumer_Price_Index", "Disposable_Income_per_Capita",
                      "Gross_regional_product", "Total_Gov._Expenditure", "Disposable_Income_per_Capita_Rural_lag", "Disposable_Income_per_Capita_Rural", "Forest_Coverage_Rate",
                      "Rural_Population") -> incl_ind
-#
+
+# incl ind with controls
+!colnames(df_panel) %in% c("Region", "Geom", "year", "Number_of_Beds_in_Health_Care_Institutions",
+                           "Number_of_Beds_in_Hospitals", "Number_of_Health_Care_Institutions", "Number_of_Medical_Personell",
+                           "Sample_population_of_age_0_14", "Sample_population_of_age_65_and_older", "Waste_Gas_Emissions_Smoke_and_Dust", "CPI_Health_Care",
+                           "Health_Care_Expenditures", "Consumer_Price_Index", "Disposable_Income_per_Capita",
+                           "Gross_regional_product", "Total_Gov._Expenditure", "Rural_Population", "Sample_population_of_age_15_65", "Disposable_Income_per_Capita_Rural", 
+                           "Disposable_Income_per_Capita_Rural_lag", "Disposable_Income_per_Capita_Urban_lag") -> incl_ind
+
+# incl ind with controls
+!colnames(df_panel) %in% c("Region", "Geom", "year", "Number_of_Beds_in_Health_Care_Institutions",
+                           "Number_of_Beds_in_Hospitals", "Number_of_Health_Care_Institutions", "Number_of_Medical_Personell",
+                           "Sample_population_of_age_0_14", "Sample_population_of_age_65_and_older", "Waste_Gas_Emissions_Smoke_and_Dust", "CPI_Health_Care",
+                           "Health_Care_Expenditures", "Consumer_Price_Index", "Disposable_Income_per_Capita",
+                           "Gross_regional_product", "Total_Gov._Expenditure", "Rural_Population", "Sample_population_of_age_15_65", "Disposable_Income_per_Capita_Rural", 
+                           "Disposable_Income_per_Capita_Rural_lag", "Disposable_Income_per_Capita_Urban_lag", "Waste_Gas_Emissions_Nitrogen", "Waste_Gas_Emissions_Nitrogen_lag",
+                           "Waste_Gas_Emissions_Particular_Matter", "Waste_Gas_Emissions_Particular_Matter_lag") -> incl_ind
+
+# incl ind with controls
+# !colnames(df_panel) %in% c("Region", "Geom", "year", "Number_of_Beds_in_Health_Care_Institutions",
+#                            "Number_of_Beds_in_Hospitals", "Number_of_Health_Care_Institutions", "Number_of_Medical_Personell",
+#                            "Sample_population_of_age_0_14", "Sample_population_of_age_65_and_older", "Waste_Gas_Emissions_Smoke_and_Dust", "CPI_Health_Care",
+#                            "Health_Care_Expenditures", "Consumer_Price_Index", "Disposable_Income_per_Capita",
+#                            "Gross_regional_product", "Total_Gov._Expenditure", "Waste_Gas_Emissions_Nitrogen", "Waste_Gas_Emissions_Sulphur", "Waste_Gas_Emissions_Nitrogen_lag",
+#                            "Waste_Gas_Emissions_Sulphur_lag", "Disposable_Income_per_Capita_Rural", "Forest_Coverage_Rate", "Disposable_Income_per_Capita_Rural_lag") -> incl_ind
+
 # NAs
 sapply(df_panel, \(cols) complete.cases(cols) |> all()) -> miss_ind
 df_panel[, "Population_affected_by_Naural_Desasters"] |> is.na() -> ind
@@ -79,10 +104,42 @@ rVCV <- plm::vcovHC(panel_SLX, method = "arellano")
 
 # coeftest
 corrected_res <- lmtest::coeftest(x = panel_SLX, vcov = rVCV)
+
 # lmtest::coeftest(panel_SDE)
 corrected_res
 
 # table
-#stargazer::stargazer(corrected_res)
+# stargazer::stargazer(corrected_res)
 
+######################## variable transformation ##############################
 
+# formula 
+fml_string_transform  <- colnames(df_panel)[incl_ind & miss_ind]
+
+# vars not to log
+ind_sample_pop <- grepl("15_65", fml_string_transform, fixed = TRUE)
+
+# pick out all vars to log transform 
+fml_log_inputs <- paste0("log(", fml_string_transform[!ind_sample_pop], ")")
+
+# final log transformed formula
+fml_log_transformed <- paste("log(Health_Care_Expenditures) ~", paste(fml_string_transform[ind_sample_pop], paste(fml_log_inputs, collapse = " + "), sep = " + ")) |> as.formula()
+ 
+# fit with new model spec
+panel_SLX_log <- plm::plm(fml_log_transformed , data = df_panel, effect = "individual", model = "within") 
+
+# residual plot
+panel_SLX_log$residuals |> plot()
+
+# BP and PDW
+plm::pdwtest(panel_SLX_log) # test for serial correlation #significant
+lmtest::bptest(panel_SLX_log)
+
+#VCV
+rVCV_log <- plm::vcovHC(panel_SLX_log, method = "arellano")
+
+# coeftest
+corrected_res_log <- lmtest::coeftest(x = panel_SLX_log, vcov = rVCV_log)
+
+# return
+corrected_res_log
